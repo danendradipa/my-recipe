6fd911b30718c58993b228578ee32a5fc5e9f38b
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-01-21 15:09:40 +0700

    refactor: standardize formatting in index.ts for improved readability

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,29 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: () => import('../views/Home.vue')
+      path: "/",
+      name: "Home",
+      component: () => import("../views/Home.vue"),
     },
     {
-      path: '/recipes',
-      name: 'Recipes',
-      component: () => import('../views/Recipes.vue')
+      path: "/recipes",
+      name: "Recipes",
+      component: () => import("../views/Recipes.vue"),
     },
     {
-      path: '/recipes/:id',
-      name: 'RecipeDetail',
-      component: () => import('../views/RecipeDetail.vue')
+      path: "/recipes/:id",
+      name: "RecipeDetail",
+      component: () => import("../views/RecipeDetail.vue"),
     },
     {
-      path: '/about',
-      name: 'About',
-      component: () => import('../views/About.vue')
+      path: "/about",
+      name: "About",
+      component: () => import("../views/About.vue"),
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
     }
-  ]
-})
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    }
+    return { top: 0 };
+  },
+});
 
-export default router
+export default router;
