362ca93a2b818ea063161bfbec0230fee4f8fc7e
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-01-21 15:20:02 +0700

    feat: add RecipeMetaCard component for displaying recipe metadata

---
+++ src/components/recipe/RecipeMetaCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  label: string
+  value: string | number
+}>()
+</script>
+
+<template>
+  <div class="bg-white p-4 rounded-lg shadow text-center">
+    <div class="text-3xl mb-2">{{ icon }}</div>
+    <div class="text-sm text-gray-600">{{ label }}</div>
+    <div class="text-lg font-semibold">{{ value }}</div>
+  </div>
+</template>
